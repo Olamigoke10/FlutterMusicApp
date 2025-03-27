cdc29f82467043d6ab2368e3b7b73627e3d51e4d
MRBOX <olamigokeadebayo90@gmail.com>
2025-03-27 02:33:55 +0100

    Adding the controllers to the form

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -11,49 +11,70 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose(){
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body:  Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Sign Up.',
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 30,),
-            const CustomField(
-              hintText: "Name",
-            ),
-            const SizedBox(height: 15,),
-            const CustomField(
-              hintText: "Email",
-            ),
-            const SizedBox(height: 15,),
-            const CustomField(
-              hintText: "Password",
-            ),
-            const SizedBox(height: 20,),
-            const AuthGradientButton(),
-            const SizedBox(height: 20,),
-            RichText(
-              text: TextSpan(
-                text: 'Already have an account? ',
-                style: Theme.of(context).textTheme.titleMedium,
-                children: [
-                  TextSpan(
-                    text: "Sign In",
-                    style: TextStyle(
-                      color: Pallete.gradient2,
-                      fontWeight: FontWeight.bold
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign Up.',
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30,),
+              CustomField(
+                hintText: "Name",
+                controller: nameController,
+              ),
+              const SizedBox(height: 15,),
+              CustomField(
+                hintText: "Email",
+                controller: emailController,
+              ),
+              const SizedBox(height: 15,),
+              CustomField(
+                hintText: "Password",
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              const SizedBox(height: 20,),
+              const AuthGradientButton(),
+              const SizedBox(height: 20,),
+              RichText(
+                text: TextSpan(
+                  text: 'Already have an account? ',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: "Sign In",
+                      style: TextStyle(
+                        color: Pallete.gradient2,
+                        fontWeight: FontWeight.bold
+                      )
                     )
-                  )
-                ]
-            ))
-          ],
+                  ]
+              ))
+            ],
+          ),
         ),
       ),
     );
